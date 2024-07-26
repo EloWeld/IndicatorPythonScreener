ca29599d8ba0731a8edecc31c56d043324b3047e
EloWeld <51724400+EloWeld@users.noreply.github.com>
2024-07-26 17:36:15 +0300

    remove cache when flag

--- main_dash_version.py
+++ main_dash_version.py
@@ -169,4 +169,6 @@ def update_graph(data):
 
 if __name__ == "__main__":
     open_url(f"http://127.0.0.1:{settings['service_port']}")
+    if settings.get('save_cache', False):
+        save_cached(settings, [])
     app.run_server(debug=False, port=settings['service_port'])
